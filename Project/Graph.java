66d2158370daf9976768c9aff89924a734b929e1
temuujin1324 <105218513+temuujin1324@users.noreply.github.com>
2022-12-05 14:08:34 -0500

    Add files via upload

---
+++ Project/Graph.java
@@ -0,0 +1,117 @@
+package Project;
+
+public class Graph {
+	private static final int INF = Integer.MAX_VALUE; // INF represents the infinite weight
+	protected int[][] W; // the adjacent matrix
+	protected int n;
+
+	public Graph(int num_vertices) {
+		n = num_vertices;
+		W = new int[n][n];
+
+		// Initialize the adjacency matrix
+		for (int i = 0; i < n; i++)
+			for (int j = 0; j < n; j++)
+				if (i == j)
+					W[i][j] = 0;
+				else
+					W[i][j] = INF;
+	}
+
+	// Accessors
+	// Returns the number of the vertices
+	public int NumberOfVertices() {
+		return n;
+	}
+
+	// Returns a copy of the adjacency matrix
+	public int[][] getCopyOfMatrix() {
+		int[][] answer = new int[n][n];
+		for (int i = 0; i < n; i++)
+			for (int j = 0; j < n; j++)
+				answer[i][j] = W[i][j];
+		return answer;
+	}
+
+	// Returns the weight of edge (u, v)
+	public int getWeightOf(int u, int v) {
+		return W[u][v];
+	}
+
+	// Returns the sum of two weights
+	// Don't directly add two weights or you could have an overflow (for example,
+	// 1+INF)
+	// Use this static method to add two weights, getWeightSum(weight1, weight2)
+	public static int getWeightSum(int a, int b) {
+		if (a == INF || b == INF)
+			return INF;
+		return a + b;
+	}
+
+	// Returns the weight of edge (u, v)
+	public int compare(int a, int b) {
+		if (a == INF) {
+			if (b == INF)
+				return 0;
+			return 1;
+		} else {
+			if (b == INF)
+				return -1;
+
+			if (a > b)
+				return 1;
+			else if (a < b)
+				return -1;
+			return 0;
+		}
+	}
+
+	// Returns the weight of edge (u, v)
+	public static int INF() {
+		return INF;
+	}
+
+	// Prints the adjacency matrix
+	public void printAdjacentMatrix() {
+		printMatrix("Adjacent Matrix", W);
+	}
+
+	// Prints the general matrix
+	public void printMatrix(String name, int[][] a) {
+		System.out.println(name + " = ");
+		for (int i = 0; i < n; ++i) {
+			for (int j = 0; j < n; ++j) {
+				if (a[i][j] == INF)
+					System.out.print("  INF");
+				else
+					System.out.format("%5d", a[i][j]);
+			}
+			System.out.println();
+		}
+	}
+
+	// Modifiers
+	public void addEdge(int u, int v, int weight) {
+		if (u < 0 || u >= n || v < 0 || v >= n)
+			throw new IllegalArgumentException("Illegel Vertex Index!");
+		if (u == v)
+			return;
+		W[u][v] = weight;
+	}
+
+	public void setAllZeroWeights() {
+		for (int i = 0; i < n; i++)
+			for (int j = 0; j < n; j++)
+				W[i][j] = 0;
+	}
+
+	// ===========================================================
+	public static void main(String[] args) {
+		System.out.println(Graph.INF);
+		System.out.println(Graph.INF - 1);
+		System.out.println(Graph.INF + 1);
+	}
+
+	public void addUndirectedEdge(Integer integer, Integer integer2, int i) {
+	}
+}
---
+++ Project/adjacency.java
@@ -0,0 +1,234 @@
+package Project;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+import javax.lang.model.SourceVersion;
+import javax.sql.rowset.spi.SyncResolver;
+import javax.xml.crypto.Data;
+import javax.xml.transform.Source;
+
+import ch9.priorityqueue.Entry;
+
+public class adjacency {
+    static HashMap<String, Integer> Dict = new HashMap<>();
+    static HashMap<Integer, ArrayList<Integer>> Pairs = new HashMap<>();
+    static HashMap<Integer, String> ReverseDict = new HashMap<>();
+
+    static String Path = "/Users/temkaisea/Desktop/code/java/Project/routes.csv";
+
+    public static int rownumber() throws IOException {
+        String Line = "";
+
+        BufferedReader br = new BufferedReader(new FileReader(Path));
+
+        int i = 0;
+        while ((Line = br.readLine()) != null) {
+            if (Line.contains("\\N")) {
+                continue;
+            }
+            i++;
+        }
+
+        return i;
+    }
+
+    public static Integer[] SourceAirportdata() throws IOException {
+        int n = rownumber();
+        String Line = "";
+
+        BufferedReader br = new BufferedReader(new FileReader(Path));
+        int i = 0;
+        Integer[] SourceAirport = new Integer[n];
+        while ((Line = br.readLine()) != null) {
+            String[] Values = Line.split(",");
+            if (Line.contains("\\N")) {
+                continue;
+            }
+            SourceAirport[i] = Integer.parseInt(Values[3]);
+
+            i++;
+
+        }
+        return SourceAirport;
+
+    }
+
+    public static Integer[] DestinationAirportdata() throws IOException {
+        int n = rownumber();
+        String Line = "";
+        BufferedReader br = new BufferedReader(new FileReader(Path));
+        int i = 0;
+        Integer[] DestinationAirport = new Integer[n];
+        while ((Line = br.readLine()) != null) {
+
+            String[] Values = Line.split(",");
+            if (Line.contains("\\N")) {
+                continue;
+            }
+
+            DestinationAirport[i] = Integer.parseInt(Values[5]);
+
+            i++;
+
+        }
+
+        return DestinationAirport;
+
+    }
+
+    public static Integer[] reIndexdestination() throws IOException {
+        int n = rownumber();
+
+        Integer[] DestinationAirport = DestinationAirportdata();
+        for (int i = 0; i < n; i++) {
+            DestinationAirport[i] = i;
+        }
+        return DestinationAirport;
+    }
+
+    public static Integer[] reIndexsource() throws IOException {
+        int n = rownumber();
+
+        Integer[] SourceAirport = SourceAirportdata();
+        for (int i = 0; i < n; i++) {
+            SourceAirport[i] = i;
+        }
+        return SourceAirport;
+    }
+
+    public static String[] SourceNaming() throws IOException {
+        int n = rownumber();
+        String[] SourceName = new String[n];
+        for (int i = 0; i < n; i++) {
+            SourceName[i] = "";
+        }
+        String Line = "";
+        BufferedReader br = new BufferedReader(new FileReader(Path));
+
+        int i = 0;
+        while ((Line = br.readLine()) != null) {
+            String[] Values = Line.split(",");
+
+            if (Line.contains("\\N")) {
+                continue;
+            }
+            SourceName[i] = Values[2];
+
+            i++;
+
+        }
+        return SourceName;
+    }
+
+    public static String[] DestinationNaming() throws IOException {
+        int n = rownumber();
+        String[] SourceName = new String[n];
+        for (int i = 0; i < n; i++) {
+            SourceName[i] = "";
+        }
+        String Line = "";
+        BufferedReader br = new BufferedReader(new FileReader(Path));
+
+        int i = 0;
+        while ((Line = br.readLine()) != null) {
+            String[] Values = Line.split(",");
+
+            if (Line.contains("\\N")) {
+                continue;
+            }
+            SourceName[i] = Values[4];
+
+            i++;
+
+        }
+        return SourceName;
+    }
+
+    public static HashMap CreateDict() throws IOException {
+        // DICT STARTS AT 1
+        // add the airports to the dict and award them indexes.
+
+        String Line = "";
+        BufferedReader br = new BufferedReader(new FileReader(Path));
+        int i = 1;
+        while ((Line = br.readLine()) != null) {
+            String[] Values = Line.split(",");
+
+            if (Line.contains("\\N")) {
+                continue;
+            }
+            if (Dict.containsKey(Values[2])) {
+                continue;
+            }
+            Dict.put(Values[2], i);
+            // Name Then INT
+
+            i++;
+
+        }
+        return Dict;
+
+    }
+
+    public static HashMap CreatePairs() throws IOException {
+        // create route pairs such that first index is the index of source and the
+        // second is destination
+        String Line = "";
+        BufferedReader br = new BufferedReader(new FileReader(Path));
+
+        while ((Line = br.readLine()) != null) {
+            ArrayList<Integer> myArray = new ArrayList<Integer>();
+
+            String[] Values = Line.split(",");
+            if (Line.contains("\\N")) {
+                continue;
+            }
+
+            if (Values[4] == "" || Values[2] == "") {
+                continue;
+            }
+            myArray.add(Dict.get(Values[4]));
+            if (Pairs.containsKey(Dict.get(Values[2]))) {
+                myArray = Pairs.get(Dict.get(Values[2]));
+                myArray.add(Dict.get(Values[4]));
+            }
+            Pairs.put(Dict.get(Values[2]), myArray);
+
+        }
+
+        return Pairs;
+
+    }
+
+    public static HashMap CreateReverseDict() throws IOException {
+        for (Map.Entry<String, Integer> entry : Dict.entrySet()) {
+
+            String key = entry.getKey();
+            Integer value = entry.getValue();
+            ReverseDict.put(value, key);
+
+        }
+        return ReverseDict;
+
+    }
+
+    public static void main(String[] args) throws IOException {
+        CreateDict(); // AIRPORT ID = INTEGER
+        CreatePairs(); // INTEGER = INTEGER
+        CreateReverseDict();
+
+        for (int i = 0; i < Pairs.get(Dict.get("AER")).size(); i++) {
+            System.out.println(ReverseDict.get(Pairs.get(Dict.get("AER")).get(i)));
+        }
+
+    }
+}
